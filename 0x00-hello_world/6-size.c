425dd730a7eeb9c2850d36971d8d893343eaa9e5
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-13 13:37:35 +0000

    End of mandatory task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %lu byte(s)\n", sizeof(a));
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", sizeof(b));
-	printf("Size of long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(d));
-	printf("Size of float: %lu byte(s)\n", sizeof(f));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
