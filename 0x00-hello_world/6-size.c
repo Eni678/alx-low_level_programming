9eb2ed6b5a2b7b7074b498fc2bc5be0389108eb5
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-13 12:57:21 +0000

    End of mandatory task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,6 @@ int main(void)
 	printf("Size of int: %zu byte(s)\n", sizeof(int));
 	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
