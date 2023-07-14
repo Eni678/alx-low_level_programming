ec657871ae9f97900c5e5dec861ccc1fd3d88dea
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-14 14:17:56 +0000

    Task 13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,23 +9,20 @@ int main(void)
 {
 	int k, l;
 
-	for (k = '0'; k <= 98; k++)
+	for (k = 0; k <= 98; k++)
 	{
 		for (l = k + 1; l <= 99; l++)
 		{
-			if (k != l)
-			{
-				putchar((k / 10) + '0');
-				putchar((k % 10) + '0');
-				putchar(' ');
-				putchar((l / 10) + '0');
-				putchar((l % 10) + '0');
-				if (k != 98 || l != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((k / 10) + '0');
+			putchar((k % 10) + '0');
+			putchar(' ');
+			putchar((l / 10) + '0');
+			putchar((l % 10) + '0');
+
+			if (k == 98 && l == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
