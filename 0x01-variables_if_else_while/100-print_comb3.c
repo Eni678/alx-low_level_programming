283f68bc8adaa3d5eb9745fb0f019284273ae8bb
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-14 13:24:26 +0000

    task 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			if (b != a)
+			{
+				putchar(a);
+				putchar(b);
+			if (a == '8' && b == '9')
+
+				continue;
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
