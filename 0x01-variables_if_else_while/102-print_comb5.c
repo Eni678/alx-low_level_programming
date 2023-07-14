3c47b714e3acfe03bd9734a8267360aeecbdbd9e
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-14 13:54:06 +0000

    Task 13

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k, l;
+
+	for (k = '0'; k <= 98; k++)
+	{
+		for (l = k + 1; l <= 99; l++)
+		{
+			putchar((k / 10) + '0');
+			putchar((k % 10) + '0');
+			putchar(' ');
+			putchar((l / 10) + '0');
+			putchar((l % 10) + '0');
+
+			if (k == 98 && l == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
