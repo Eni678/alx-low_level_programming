d21daeea0b91949d75525db8636b4846b9674797
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-14 11:34:59 +0000

    Task 8

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,11 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int d;
+
+	for (d = 0; d <= 9; d++)
+		putchar(d);
+
 	putchar('\n');
 
 	return (0);
