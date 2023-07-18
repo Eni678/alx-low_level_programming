e49492d394420ca9b9669c899246c0b7e6435a93
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-18 20:24:02 +0000

    tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,28 +9,19 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
-		for (; n <= 98; n++)
-		{
-			printf("%d ", n);
-
-			if (n == 98)
-				continue;
-			printf(", ");
-		}
-		printf("\n");
-	}
-	else
-	{
-		for (; n >= 98; n--)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
-
-			if (n == 98)
-				continue;
-			printf(", ");
 		}
 	}
-	printf("\n");
+	else if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
 }
