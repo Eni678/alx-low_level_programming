83c21eaf5527e03ecd6e5bb40b327b4ace0bab60
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-18 14:21:07 +0000

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}
