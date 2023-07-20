b20b94def2beb0b994d7f82a890865ec617bb614
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-20 20:37:22 +0000

    abeg

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
 int _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -14,6 +12,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
 
-#endif /* MAIN_H */
+#endif
