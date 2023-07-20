990d19837d41624a086c54ae8604636e66da73f9
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-20 21:43:14 +0000

    m

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * print_integer - Prints an integer to the standard output
- *
+ * print_number - Prints an integer to the standard output
  * @n: The integer to print
- *
- * This function takes an integer @n as input and prints it to the standard
- * output using that printf function. The integer is printed  as a decimal
- * number with no leading zeros or sign
- * print_number - this function prints out the num
- * Return: None
+ * Return: Nothing!
  */
 void print_number(int n)
 {
-	int num = n;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
+		n *= -1;
+		k = n;
 		_putchar('-');
-		num = -num;
 	}
-	if (n / 10 > 0)
-	{
-		print_number(num / 10);
-	}
-	_putchar((num % 10) + 48);
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
 }
