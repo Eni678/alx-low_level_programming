56f010b70a26970c62a7f0d539debf14a7304284
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-20 21:04:01 +0000

    hh

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 /**
  * _isupper - this function checks for uppercase characters
  * @c: the character to be checked
- *
  * Return: 1 if the character is uppercase or 0 if otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
-	else
+	}
 		return (0);
 }
