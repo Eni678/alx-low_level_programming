98e38d622de0449544834cc4d36226c7e369822c
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-24 20:32:36 +0000

    oshori

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,14 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int len = 0, i = 0;
 
-	while (str[i] != '\0')
-	{
+	while (str[len] != '\0')
+		len++;
+
+	len -= 1;
+
+	for (; i <= len; i += 2)
 		_putchar(str[i]);
-		i += 2;
-	}
 	_putchar('\n');
 }
