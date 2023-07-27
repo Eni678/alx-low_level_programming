1969ef0f0c11875cd8e3ec1b01cbd34ab291d701
Enilama_Oshoriamhe <enilamaoshoriamhe687@gmail.com>
2023-07-27 21:49:03 +0000

    late

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,16 +10,16 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot13_alphabet[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == alphabet[j])
+			if (s[i] == data1[j])
 			{
-				s[i] = rot13_alphabet[j];
+				s[i] = datarot[j];
 				break;
 			}
 		}
